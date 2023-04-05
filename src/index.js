cabe8ecadc0d584b4445b22cdadfb5bf731ab267
Serhii Pradid <115069705+Serhii-Pradid@users.noreply.github.com>
2023-04-05 10:27:21 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 
-const searchFormEl = document.querySelector('.search-form');
-const loadMoreBtn = document.querySelector('.load-more');
-const listGallery = document.querySelector('.gallery');
+const searchFormEl = document.querySelector(".search-form");
+const loadMoreBtn = document.querySelector(".load-more");
+const listGallery = document.querySelector(".gallery");
+
+const lightbox = new SimpleLightbox('.gallery a', {});
+
 
 let query = '';
 let page = 1;
@@ -14,35 +17,61 @@ const perPage = 40;
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
 
+function clearMarkup() {
+    listGallery.innerHTML = '';
+   }
+
+
 function handleSearchFormSubmit(event) {
     event.preventDefault();
 
-    
-    const searchQuery = formData.get('searchQuery').trim();
+     query = event.currentTarget.elements.searchQuery.value.trim();
 
-    if(searchQuery === '') {
-        loadMoreBtn.classList.toggle('is-hidden');
-        return Notify.failure('Please enter a title!')
-    }
+    clearMarkup();
 
+    if (!query) {
+        //loadMoreBtn.classList.add('is-hidden');
+        return Notify.failure('Please enter a title!');
+        }
 
+    fetchImage(query, page, perPage)
+    .then(data => {
+    console.log(data);
+    if(data.totalHits === 0) {
+    Notify.failure("Sorry, there are no images matching your search query. Please try again."
+    );
+    return;
+    }   
+
+    Notify.success(`"Hooray! We found ${data.totalHits} images."`);
+    createMarkup(data.hits);
+ })
+.catch(error => console.log(error))
+.finally(() => {
+  searchFormEl.reset();
+});
 }
 
-function createImageMarkup(image) {
+function createMarkup(data){
+    const markupList = createImageMarkup(data);
+    listGallery.insertAdjacentHTML('beforeend' , markupList )
+}
 
-    return image.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => 
-    
-    `<a href="${largeImageURL}"></a>
+function createImageMarkup(images) {
+    return images.map(
+    ({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
+    return `
     <div class="photo-card">
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+     <a href="${largeImageURL}">
+     <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+     </a>
     <div class="info">
     <p class="info-item"><b>Likes</b>${likes}</p>
     <p class="info-item"><b>Views</b>${views}</p>
     <p class="info-item"><b>Comments</b>${comments}</p>
     <p class="info-item"><b>Downloads</b>${downloads}</p>
   </div>
-</div>
-</a>`
-)
-
+</div>`
+    })
+    .join("")
 }
